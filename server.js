625dc0354b06efb862a9ef578763a5ef6b89e5eb
SamsonMokaya <mokayasamson950@gmail.com>
2023-06-02 13:37:40 +0300

    Connecting to the mongoose DB

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
-const errorHandler = require("./middleware/errorHandler")
+const errorHandler = require("./middleware/errorHandler");
+const connectDb = require("./config/dbConnection");
 
+connectDb();
 const app = express();
 
 const port = process.env.PORT || 5000;
 
 app.use(express.json())
 app.use("/api/contacts", require("./routes/contactRoutes"));
+
 app.use(errorHandler)
 
 app.listen(port, ()=> {
